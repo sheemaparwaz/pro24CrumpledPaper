300e5914521c71913399487aa4095c230828d56d
Sheema Parwaz <sheemaparwaz@gmail.com>
2020-10-04 01:30:38 +0530

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,25 +1,37 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj, paperObject,groundObject	
+var world;
 
-function preload()
-{
-	
-}
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
+	
 
-	//Create the Bodies Here.
-
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
 
 	Engine.run(engine);
+	//Render.run(render);
   
 }
 
@@ -27,10 +39,22 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+ 
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
   
-  drawSprites();
+ 
+  
+  
  
 }
 
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
 
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+}
 
